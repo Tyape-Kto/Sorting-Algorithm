61b15521e22dda4a017ac01e57de63355feb9123
Tyape-Kto <55008900+Tyape-Kto@users.noreply.github.com>
2021-10-12 07:47:48 +0545

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,6 +1,9 @@
 from random import randint
-### This sorting algorithm sorts from backwards [Picchadi bate mildai aauxa]
+
 def Bubble(arr):
+  """
+    This checks 2 element each time and checks for len(arr) times [ Picchadi bate mildai aau6 ] { TimeComplexity: O(n^2) }
+  """
   for j in range(len(arr)):
     for i in range(len(arr)-j-1):
       if arr[i] > arr[i+1]:
@@ -8,6 +11,9 @@ def Bubble(arr):
   return arr
 
 def Insertion(arr):
+  """
+    This checks one element to every then moves on, sorts from forward [first element bate mildai aau6] { O(n^2) } 
+  """
   for i in range(len(arr)):
     for j in range(i+1, len(arr)):
       if arr[i] > arr[j]:
