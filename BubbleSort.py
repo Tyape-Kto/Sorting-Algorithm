e39f01d7c8b2dfc94de1cabf8c8677f1f10b07f6
Tyape-Kto <55008900+Tyape-Kto@users.noreply.github.com>
2021-10-12 07:36:15 +0545

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,9 +1,11 @@
-#Bubble-Sorting
 from random import randint
 
 def Bubble(arr):
-  for i in range(len(arr)):
-    print(arr[i])
+  for j in range(len(arr)):
+    for i in range(len(arr)-j-1):
+      if arr[i] > arr[i+1]:
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+  return arr
 
 l = [randint(1,20) for _ in range(10)]
 Bubble(l)
