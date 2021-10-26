3a465db8c3992bc2bee578dd5504893da64f6c16
Tyape-Kto <55008900+Tyape-Kto@users.noreply.github.com>
2021-10-26 16:19:19 +0545

    Update Udemy_sorting.py

--- Udemy_sorting.py
+++ Udemy_sorting.py
@@ -9,5 +9,29 @@ def bubble_sort(arr):
         swap_happened = True
   return arr
 
+def merge(left, right):
+  sorted_list = []
+  i, j = 0, 0
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      sorted_list.append(left[i])
+      i += 1
+    else:
+      sorted_list.append(right[j])
+      j += 1
+  while i < len(left):
+    sorted_list.append(left[i])
+    i += 1
+  while j < len(right):
+    sorted_list.append(right[j])
+    j += 1
+  return sorted_list
+
 def merge_sort(arr):
-  pass
+  if len(arr) < 2:
+    return arr[:]
+  else:
+    middle = len(arr) // 2
+    l = merge_sort(arr[:middle])
+    r = merge_sort(arr[middle:])
+    return merge(l, r)
